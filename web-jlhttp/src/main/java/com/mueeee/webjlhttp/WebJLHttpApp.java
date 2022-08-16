f1c3a13718760385785353883eee9a968ea85e4f
westinyang <517858177@qq.com>
2022-08-16 11:13:25 +0800

    refactor: String concatenation modified to text block

--- web-jlhttp/src/main/java/com/mueeee/webjlhttp/WebJLHttpApp.java
+++ web-jlhttp/src/main/java/com/mueeee/webjlhttp/WebJLHttpApp.java
@@ -18,52 +18,53 @@ public class WebJLHttpApp {
         HTTPServer.VirtualHost host = httpServer.getVirtualHost(null);  // default virtual host
 
         host.addContext("/", (req, resp) -> {
-            String html =
-                    "<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    "<head>\n" +
-                    "    <meta charset=\"UTF-8\">\n" +
-                    "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
-                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                    "    <title>JLHTTP</title>\n" +
-                    "</head>\n" +
-                    "<body>\n" +
-                    "    <h1>JLHTTP，嵌入式 Java HTTP 服务器</h1>\n" +
-                    "    <ul>\n" +
-                    "        <li><a href=\"/\">/</a></li>\n" +
-                    "        <li><a href=\"/text\">/text</a></li>\n" +
-                    "        <li><a href=\"/json\">/json</a></li>\n" +
-                    "    </ul>\n" +
-                    "   <script>\n" +
-                    "        var autoclose = getUrlParam(\"autoclose\");\n" +
-                    "        if (autoclose) {\n" +
-                    "            checkIfServerIsDown();\n" +
-                    "        }\n" +
-                    "        // 检查服务器是否关闭，如果关闭则自动关闭当前页签\n" +
-                    "        function checkIfServerIsDown() {\n" +
-                    "            setInterval(function () {\n" +
-                    "                var xhr = new XMLHttpRequest();\n" +
-                    "                xhr.open('GET', \"/\", true);\n" +
-                    "                xhr.onreadystatechange = function () {\n" +
-                    "                    if (xhr.readyState === 4 && xhr.status === 200 || xhr.status === 304) {\n" +
-                    "                        var data = xhr.responseText;\n" +
-                    "                    }\n" +
-                    "                };\n" +
-                    "                xhr.onerror = function (e) {\n" +
-                    "                    open(location, '_self').close();\n" +
-                    "                };\n" +
-                    "                xhr.send();\n" +
-                    "            }, 1000);\n" +
-                    "        }\n" +
-                    "        // 获取网址参数\n" +
-                    "        function getUrlParam (name) {\n" +
-                    "            var reg = new RegExp(\"(^|&)\" + name + \"=([^&]*)(&|$)\");\n" +
-                    "            var r = window.location.search.substr(1).match(reg);\n" +
-                    "            if (r != null) return unescape(r[2]); return null;\n" +
-                    "        }\n" +
-                    "    </script>" +
-                    "</body>\n" +
-                    "</html>";
+            String html = """
+                    <!DOCTYPE html>
+                        <html>
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>JLHTTP</title>
+                        </head>
+                        <body>
+                            <h1>JLHTTP，嵌入式 Java HTTP 服务器</h1>
+                            <ul>
+                                <li><a href="/">/</a></li>
+                                <li><a href="/text">/text</a></li>
+                                <li><a href="/json">/json</a></li>
+                            </ul>
+                            <script>
+                                var autoclose = getUrlParam("autoclose");
+                                if (autoclose) {
+                                    checkIfServerIsDown();
+                                }
+                                // 检查服务器是否关闭，如果关闭则自动关闭当前页签
+                                function checkIfServerIsDown() {
+                                    setInterval(function () {
+                                        var xhr = new XMLHttpRequest();
+                                        xhr.open('GET', "/", true);
+                                        xhr.onreadystatechange = function () {
+                                            if (xhr.readyState === 4 && xhr.status === 200 || xhr.status === 304) {
+                                                var data = xhr.responseText;
+                                            }
+                                        };
+                                        xhr.onerror = function (e) {
+                                            open(location, '_self').close();
+                                        };
+                                        xhr.send();
+                                    }, 1000);
+                                }
+                                // 获取网址参数
+                                function getUrlParam (name) {
+                                    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+                                    var r = window.location.search.substr(1).match(reg);
+                                    if (r != null) return unescape(r[2]); return null;
+                                }
+                            </script>
+                        </body>
+                    </html>
+                    """;
             resp.getHeaders().add("Content-Type", "text/html; charset=utf-8");
             resp.send(200, html);
             return 1;
@@ -75,12 +76,13 @@ public class WebJLHttpApp {
             return 1;
         });
         host.addContext("/json", (req, resp) -> {
-            String data =
-                    "{\n" +
-                    "    \"code\": 0,\n" +
-                    "    \"message\": \"success\",\n" +
-                    "    \"data\": {}\n" +
-                    "}";
+            String data = """
+                    {
+                        "code": 0,
+                        "message": "success",
+                        "data": {}
+                    }
+                    """;
             resp.getHeaders().add("Content-Type", "application/json");
             resp.send(200, data);
             return 1;
--- web-nanohttpd/src/main/java/com/mueeee/webnanohttpd/WebNanoHttpdApp.java
+++ web-nanohttpd/src/main/java/com/mueeee/webnanohttpd/WebNanoHttpdApp.java
@@ -34,63 +34,65 @@ public class WebNanoHttpdApp extends NanoHTTPD {
         Map<String, String> params = session.getParms();
 
         if ("/".equals(session.getUri())) {
-            String html =
-                    "<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    "<head>\n" +
-                    "    <meta charset=\"UTF-8\">\n" +
-                    "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
-                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                    "    <title>NanoHttpd</title>\n" +
-                    "</head>\n" +
-                    "<body>\n" +
-                    "    <h1>NanoHttpd，在 Java 中易于嵌入的小型 HTTP 服务器</h1>\n" +
-                    "    <ul>\n" +
-                    "        <li><a href=\"/\">/</a></li>\n" +
-                    "        <li><a href=\"/text\">/text</a></li>\n" +
-                    "        <li><a href=\"/json\">/json</a></li>\n" +
-                    "    </ul>\n" +
-                    "   <script>\n" +
-                    "        var autoclose = getUrlParam(\"autoclose\");\n" +
-                    "        if (autoclose) {\n" +
-                    "            checkIfServerIsDown();\n" +
-                    "        }\n" +
-                    "        // 检查服务器是否关闭，如果关闭则自动关闭当前页签\n" +
-                    "        function checkIfServerIsDown() {\n" +
-                    "            setInterval(function () {\n" +
-                    "                var xhr = new XMLHttpRequest();\n" +
-                    "                xhr.open('GET', \"/\", true);\n" +
-                    "                xhr.onreadystatechange = function () {\n" +
-                    "                    if (xhr.readyState === 4 && xhr.status === 200 || xhr.status === 304) {\n" +
-                    "                        var data = xhr.responseText;\n" +
-                    "                    }\n" +
-                    "                };\n" +
-                    "                xhr.onerror = function (e) {\n" +
-                    "                    open(location, '_self').close();\n" +
-                    "                };\n" +
-                    "                xhr.send();\n" +
-                    "            }, 1000);\n" +
-                    "        }\n" +
-                    "        // 获取网址参数\n" +
-                    "        function getUrlParam (name) {\n" +
-                    "            var reg = new RegExp(\"(^|&)\" + name + \"=([^&]*)(&|$)\");\n" +
-                    "            var r = window.location.search.substr(1).match(reg);\n" +
-                    "            if (r != null) return unescape(r[2]); return null;\n" +
-                    "        }\n" +
-                    "    </script>" +
-                    "</body>\n" +
-                    "</html>";
+            String html = """
+                    <!DOCTYPE html>
+                        <html>
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>NanoHttpd</title>
+                        </head>
+                        <body>
+                            <h1>NanoHttpd，在 Java 中易于嵌入的小型 HTTP 服务器</h1>
+                            <ul>
+                                <li><a href="/">/</a></li>
+                                <li><a href="/text">/text</a></li>
+                                <li><a href="/json">/json</a></li>
+                            </ul>
+                            <script>
+                                var autoclose = getUrlParam("autoclose");
+                                if (autoclose) {
+                                    checkIfServerIsDown();
+                                }
+                                // 检查服务器是否关闭，如果关闭则自动关闭当前页签
+                                function checkIfServerIsDown() {
+                                    setInterval(function () {
+                                        var xhr = new XMLHttpRequest();
+                                        xhr.open('GET', "/", true);
+                                        xhr.onreadystatechange = function () {
+                                            if (xhr.readyState === 4 && xhr.status === 200 || xhr.status === 304) {
+                                                var data = xhr.responseText;
+                                            }
+                                        };
+                                        xhr.onerror = function (e) {
+                                            open(location, '_self').close();
+                                        };
+                                        xhr.send();
+                                    }, 1000);
+                                }
+                                // 获取网址参数
+                                function getUrlParam (name) {
+                                    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+                                    var r = window.location.search.substr(1).match(reg);
+                                    if (r != null) return unescape(r[2]); return null;
+                                }
+                            </script>
+                        </body>
+                    </html>
+                    """;
             return newFixedLengthResponse(NanoHTTPD.Response.Status.OK, "text/html", html);
         } else if ("/text".equals(session.getUri())) {
             String text = "Hello NanoHttpd";
             return newFixedLengthResponse(NanoHTTPD.Response.Status.OK, "text/plain", text);
         } else if ("/json".equals(session.getUri())) {
-            String data =
-                    "{\n" +
-                    "    \"code\": 0,\n" +
-                    "    \"message\": \"success\",\n" +
-                    "    \"data\": {}\n" +
-                    "}";
+            String data = """
+                    {
+                        "code": 0,
+                        "message": "success",
+                        "data": {}
+                    }
+                    """;
             return newFixedLengthResponse(NanoHTTPD.Response.Status.OK, "application/json", data);
         }
 
